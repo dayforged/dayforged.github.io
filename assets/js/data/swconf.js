59be2c88a5678ae5c92a74c639f98beae9c4114b
dayforged <dayforged@users.noreply.github.com>
2025-10-18 13:53:29 +0000

    deploy: 96322983054de0945f1ab6a03256514c2d68674b

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760795552',resources: [
+    cacheName: 'chirpy-1760795609',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
